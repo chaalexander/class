2d5db90576140f0af59852b7e3218c4bf9f20ea8
sammiefrog <sammiefrog@live.com>
2020-06-02 20:40:24 -0500

    updated boolean

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+
 
 
 const apiRoutes = require("./routes/api");
